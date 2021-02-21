9f70c60a0a9917973fb1a2bd8274dbfcaa6b5659
Melvin van Megen <melvin.vmegen@gmail.com>
2021-02-21 11:50:56 +0100

    UP (projects_filled) custom images and description

--- store/index.js
+++ store/index.js
@@ -4,67 +4,112 @@ export const state = () => ({
       title: 'le bioclimatique en co-living',
       link: 'le-bioclimatique-en-co-living',
       src: 'project_1.jpg',
-      desc: 'Lorem Ipsum Lorem Ipsum',
+      desc: 'projet mémoire de fin d’étude en cours de construction … bientôt en ligne !',
       projectImages: [
-        { img: 'project_1.jpg', title: 'Création de temoignage pour les utilisateurs identifié ou non.' },
-        { img: 'project_1.jpg', title: "Mise en place d'un hub de blog avec module de recherche d'article." },
-        { img: 'project_1.jpg', title: 'Representation graphique de statistiques avec les éléments nécessaires.' },
-        { img: 'project_1.jpg', title: 'Classement des meilleurs ostéopathes par ville en fonction de leurs notations.' },
-        { img: 'project_1.jpg', title: 'Module de recherche préfiltré en fonction de la page actuelle (ici ostéopathie pour bébé filtre les ostéopathes pour bébé).' }
+        { img: 'project_1.jpg', title: 'Création de temoignage pour les utilisateurs identifié ou non.' }
+      ]
+    },
+    {
+      title: 'l’antre de l’artiste',
+      link: 'antre-de-l’artiste',
+      src: 'project_1.jpg',
+      desc: `projet en freelance de transformation d’un viel appartement en bureau / atelier. désir de créer un lieu à l’identité 
+            industrielle et actuel en gardant une âme rétro.`,
+      projectImages: [
+        { img: 'antre_de_lartiste/ART_P1.jpg', title: 'Création de temoignage pour les utilisateurs identifié ou non.' },
+        { img: 'antre_de_lartiste/ART_P2.jpg', title: "Mise en place d'un hub de blog avec module de recherche d'article." },
+        { img: 'antre_de_lartiste/ART_P3.jpg', title: 'Representation graphique de statistiques avec les éléments nécessaires.' }
       ]
     },
     {
       title: 'la théorie du jeu',
       link: 'la-théorie-du-jeu',
       src: 'project_2.jpg',
-      desc: 'Lorem Ipsum Lorem Ipsum',
+      desc: `la théorie des jeux est un projet de 4ème année d’école (réalisé en groupe de 4) de rénovation d’un bâtiment dont l’usage n’est plus 
+            défini en espace socio-culturel, actuel et accessible pour tous. Un centre social et culturel est un lieu de proximité à vocation globale, 
+            familiale et intergénérationnelle, qui accueille toute la population en veillant à la mixité sociale. C’est un lieu d’animation de la vie sociale permettant 
+            aux habitants d’exprimer, de concevoir et de réaliser leurs projets : il prend en compte l’expression des demandes et des initiatives des usagers et des habitants, 
+            et favorise la vie sociale et la vie associative. Il propose des activités ou des services à finalité sociale, éducative, culturelle ou de loisirs, 
+            ainsi que des actions spécifiques pour répondre aux problématiques sociales du territoire. 
+            Il favorise le développement des initiatives des usagers en mettant des moyens humains et logistiques à leur disposition. 
+            Pour le projet, nous devons rénover un bâtiment d’environ 3000m2 sur 2 étages. Il a fallu créer divers types d’espaces tels que : 
+            une galerie d’exposition, un café/brasserie, une bibliothèque, des lieux intimistes de travail, de réflexion & de recherche, 
+            un atelier, un fab lab, une ludothèque, des salles de musiques & de vision, des espaces techniques… Nous avons axé notre concept 
+            sur l’intergénérationnalité et la favorisation des échanges : nous avons puisé notre inspiration dans les jeux de société (notemment le monopoly), 
+            le livre donnant-donnant de robert axelrod, une vie d’écart sur canal + et enfin le jeu de la dame sur netflix. nous avons 
+            utilisé les 4 couleurs associées aux jeux : le bleu, le rouge, le jaune et le vert ainsi que le damier.`,
       projectImages: [
-        { img: 'project_1.jpg', title: 'Création de temoignage pour les utilisateurs identifié ou non.' },
-        { img: 'project_1.jpg', title: "Mise en place d'un hub de blog avec module de recherche d'article." },
-        { img: 'project_1.jpg', title: 'Representation graphique de statistiques avec les éléments nécessaires.' },
-        { img: 'project_1.jpg', title: 'Classement des meilleurs ostéopathes par ville en fonction de leurs notations.' },
-        { img: 'project_1.jpg', title: 'Module de recherche préfiltré en fonction de la page actuelle (ici ostéopathie pour bébé filtre les ostéopathes pour bébé).' }
+        { img: 'theorie_du_jeu/GABARIT1.jpg', title: 'Création de temoignage pour les utilisateurs identifié ou non.' },
+        { img: 'theorie_du_jeu/GABARIT2.jpg', title: "Mise en place d'un hub de blog avec module de recherche d'article." },
+        { img: 'theorie_du_jeu/GABARIT3.jpg', title: 'Representation graphique de statistiques avec les éléments nécessaires.' },
+        { img: 'theorie_du_jeu/GABARIT4.jpg', title: 'Classement des meilleurs ostéopathes par ville en fonction de leurs notations.' },
+        { img: 'theorie_du_jeu/GABARIT5.jpg', title: 'Classement des meilleurs ostéopathes par ville en fonction de leurs notations.' },
+        { img: 'theorie_du_jeu/GABARIT6.jpg', title: 'Module de recherche préfiltré en fonction de la page actuelle (ici ostéopathie pour bébé filtre les ostéopathes pour bébé).' }
       ]
     },
     {
       title: 'retrorama',
       link: 'retrorama',
       src: 'project_3.jpg',
-      desc: 'Lorem Ipsum Lorem Ipsum',
+      desc: `rétrorama c’est le projet de rénovation d’un ancien batiment en hostel pour la chaine ho36. c’est un projet réalisé seule, 
+            en troisième année d’école. le principe du concept est de créer un lieu au style purement rétro-futuriste, 
+            une invitation dans une autre dimension. la conception du lieu a été issue d’une réfléxion sur le temps et plus 
+            particulièrement la relation entre le passé et le futur. un espace entre réalité et fiction dans lequel cohabitent 
+            la fantaisie du passé et la magie du futur.`,
       projectImages: [
-        { img: 'retrorama_1.jpg', title: 'Création de temoignage pour les utilisateurs identifié ou non.' },
-        { img: 'retrorama_2.jpg', title: "Mise en place d'un hub de blog avec module de recherche d'article." },
-        { img: 'retrorama_3.jpg', title: 'Representation graphique de statistiques avec les éléments nécessaires.' }
+        { img: 'retrorama/retrorama_1.jpg', title: 'Création de temoignage pour les utilisateurs identifié ou non.' },
+        { img: 'retrorama/retrorama_2.jpg', title: "Mise en place d'un hub de blog avec module de recherche d'article." },
+        { img: 'retrorama/retrorama_3.jpg', title: 'Representation graphique de statistiques avec les éléments nécessaires.' },
+        { img: 'retrorama/retrorama_4.jpg', title: 'Representation graphique de statistiques avec les éléments nécessaires.' },
+        { img: 'retrorama/retrorama_5.jpg', title: 'Representation graphique de statistiques avec les éléments nécessaires.' },
+        { img: 'retrorama/retrorama_6.jpg', title: 'Representation graphique de statistiques avec les éléments nécessaires.' },
+        { img: 'retrorama/r+1 zooning.png', title: 'Representation graphique de statistiques avec les éléments nécessaires.' },
+        { img: 'retrorama/sous sol zooning copie.png', title: 'Representation graphique de statistiques avec les éléments nécessaires.' },
+        { img: 'retrorama/zooning rdc.png', title: 'Representation graphique de statistiques avec les éléments nécessaires.' }
       ]
     },
     {
       title: 'jacquemus',
       link: 'jacquemus',
       src: 'project_4.jpg',
-      desc: 'Lorem Ipsum Lorem Ipsum',
+      desc: `la cagole, c’est le projet de création de la première boutique physique de la marque française de prêt-à-porter jacquemus. 
+            le projet était orienté autour des valeurs de la marque (la provence, le sud, le soleil...). projet de troisième année d’école réalisé 
+            en groupe de 3. nous avons défini notre projet comme «simplicité sophistiquée». c’est la création d’un lieu atypique 
+            inspiré de la cagole marseillaise : éxagération, le trop, les couleurs, la sophistication, les courbes, les reflets... 
+            un maître mot : l’extravagance. nos inspirations pour ce projet ont été : le film le mépris de jean-luc godard, 
+            le documentaire cagole forever de sebastien haddouk,la bière la cagole d’yves darnaud. nous avons choisi d’utiliser 
+            des matériaux synthétiques et brillants aux couleurs acides et vitaminées : le rose (cliché), le rouge (provocant), 
+            le bleu (la mer, la piscine), le jaune (la blonde platine).`,
       projectImages: [
-        { img: 'jacquemus_1.jpg', title: 'Création de temoignage pour les utilisateurs identifié ou non.' },
-        { img: 'jacquemus_2.jpg', title: "Mise en place d'un hub de blog avec module de recherche d'article." },
-        { img: 'jacquemus_3.jpg', title: 'Representation graphique de statistiques avec les éléments nécessaires.' },
-        { img: 'jacquemus_4.jpg', title: 'Classement des meilleurs ostéopathes par ville en fonction de leurs notations.' }
+        { img: 'jacquemus/jacquemus_1.jpg', title: 'Création de temoignage pour les utilisateurs identifié ou non.' },
+        { img: 'jacquemus/jacquemus_2.jpg', title: "Mise en place d'un hub de blog avec module de recherche d'article." },
+        { img: 'jacquemus/jacquemus_3.jpg', title: 'Representation graphique de statistiques avec les éléments nécessaires.' },
+        { img: 'jacquemus/jacquemus_4.jpg', title: 'Classement des meilleurs ostéopathes par ville en fonction de leurs notations.' }
       ]
     },
     {
       title: 'dumbo stairs',
       link: 'dumbo-stairs',
       src: 'project_8.jpg',
-      desc: 'Lorem Ipsum Lorem Ipsum',
+      desc: `participation (en équipe de 2, avec justin, ingénieur) à un concours de création d’un escalier en béton, 
+            organisé par l’entreprise bpm groupe. le produit présenté est un assemblage par câbles acier de blocs de béton 
+            bfup creusés afin de créer deux marches. un bloc de 2 marches en béton d’un volume de 0,0449m3 est composé de 
+            béton fibré ayant une masse volumique de 2600 kg/m3. nous estimons donc la masse d’un module de béton à 116kg, 
+            ce qui paraît correct du point de vue de réalisation et ancrage des cables.`,
       projectImages: [
-        { img: 'dumbo_stairs_1.jpg', title: 'Création de temoignage pour les utilisateurs identifié ou non.' },
-        { img: 'dumbo_stairs_2.jpg', title: "Mise en place d'un hub de blog avec module de recherche d'article." },
-        { img: 'dumbo_stairs_3.jpg', title: 'Representation graphique de statistiques avec les éléments nécessaires.' }
+        { img: 'dumbo_stairs/dumbo_stairs_1.jpg', title: 'Création de temoignage pour les utilisateurs identifié ou non.' },
+        { img: 'dumbo_stairs/dumbo_stairs_2.jpg', title: "Mise en place d'un hub de blog avec module de recherche d'article." },
+        { img: 'dumbo_stairs/dumbo_stairs_3.jpg', title: 'Representation graphique de statistiques avec les éléments nécessaires.' }
       ]
     },
     {
-      title: 'bubble',
-      link: 'bubble',
+      title: 'Jimi Hendrix',
+      link: 'Jimi Hendrix',
       src: 'project_5.jpg',
-      desc: 'Lorem Ipsum Lorem Ipsum',
+      desc: `conception d’un lieu chaleureux dans lequel les clients peuvent se projeter. utilisation d’une oeuvre d’art représentant jimi 
+            hendrix par thomas krief. projet (réalisé en freelance) d’aménagement d’un appartement acquis en vefa. l’appartement se situe à lyon, 
+            dans le troisième arrondissement. l’appartement est déstiné à la location pour une petite famille. aménagement d’espaces verts 
+            dans le salon et sur la terrasse. technique de définition des espaces par la couleurs.`,
       projectImages: [
         { img: 'project_1.jpg', title: 'Création de temoignage pour les utilisateurs identifié ou non.' },
         { img: 'project_1.jpg', title: "Mise en place d'un hub de blog avec module de recherche d'article." },
@@ -77,25 +122,35 @@ export const state = () => ({
       title: 'revival',
       link: 'revival',
       src: 'project_6.jpg',
-      desc: 'Lorem Ipsum Lorem Ipsum',
+      desc: `projet de rénovation d’une villa d’environ 200m2 à Marcy (69) pour une famille de 4 personnes. projet réalisé 
+            seule en deuxième année d’école .la tendance mid century etait fortement souhaitée notemment par l’utilisation 
+            de matériaux nobles (cuivre, pierre, bois, pièces d’art...). logiciels utilisés pour la réalisation de ce projet : 
+            autocad, sketchup, excel.`,
       projectImages: [
-        { img: 'project_1.jpg', title: 'Création de temoignage pour les utilisateurs identifié ou non.' },
-        { img: 'project_1.jpg', title: "Mise en place d'un hub de blog avec module de recherche d'article." },
-        { img: 'project_1.jpg', title: 'Representation graphique de statistiques avec les éléments nécessaires.' },
-        { img: 'project_1.jpg', title: 'Classement des meilleurs ostéopathes par ville en fonction de leurs notations.' },
-        { img: 'project_1.jpg', title: 'Module de recherche préfiltré en fonction de la page actuelle (ici ostéopathie pour bébé filtre les ostéopathes pour bébé).' }
+        { img: 'revival/GABARIT PHOTOS3.jpg', title: 'Création de temoignage pour les utilisateurs identifié ou non.' },
+        { img: 'revival/GABARIT PHOTOS4.jpg', title: "Mise en place d'un hub de blog avec module de recherche d'article." },
+        { img: 'revival/GABARIT PHOTOS5.jpg', title: 'Representation graphique de statistiques avec les éléments nécessaires.' },
+        { img: 'revival/GABARIT PHOTOS6.jpg', title: 'Classement des meilleurs ostéopathes par ville en fonction de leurs notations.' },
+        { img: 'revival/GABARIT PHOTOS7.jpg', title: 'Module de recherche préfiltré en fonction de la page actuelle (ici ostéopathie pour bébé filtre les ostéopathes pour bébé).' },
+        { img: 'revival/GABARIT PHOTOS8.jpg', title: 'Module de recherche préfiltré en fonction de la page actuelle (ici ostéopathie pour bébé filtre les ostéopathes pour bébé).' },
+        { img: 'revival/GABARIT PHOTOS9.jpg', title: 'Module de recherche préfiltré en fonction de la page actuelle (ici ostéopathie pour bébé filtre les ostéopathes pour bébé).' },
+        { img: 'revival/GABARIT PHOTOS10.jpg', title: 'Module de recherche préfiltré en fonction de la page actuelle (ici ostéopathie pour bébé filtre les ostéopathes pour bébé).' },
+        { img: 'revival/GABARIT PHOTOS11.jpg', title: 'Module de recherche préfiltré en fonction de la page actuelle (ici ostéopathie pour bébé filtre les ostéopathes pour bébé).' }
       ]
     },
     {
       title: 'miro x mondrian',
       link: 'miro-x-mondrian',
       src: 'project_7.jpg',
-      desc: 'Lorem Ipsum Lorem Ipsum',
+      desc: `le projet ar(t)chitecture consistait en la rénovation d’un appartement déstiné à une location de courte durée. 
+             le maître d’ouvrage souhaitait un aménagement des plus atypiques.projet réalisé seule, en deuxième année d’école. 
+             j’ai pris la décision de trouver un fil conducteur à mon projet et d’en faire un lieu avant-gardiste, unique. 
+             mon inspiration proveneait des galeries d’art et du peintre joan miro. `,
       projectImages: [
-        { img: 'miro_1.jpg', title: 'Création de temoignage pour les utilisateurs identifié ou non.' },
-        { img: 'miro_2.jpg', title: "Mise en place d'un hub de blog avec module de recherche d'article." },
-        { img: 'miro_3.jpg', title: 'Representation graphique de statistiques avec les éléments nécessaires.' },
-        { img: 'miro_4.jpg', title: 'Classement des meilleurs ostéopathes par ville en fonction de leurs notations.' }
+        { img: 'miro/miro_1.jpg', title: 'Création de temoignage pour les utilisateurs identifié ou non.' },
+        { img: 'miro/miro_2.jpg', title: "Mise en place d'un hub de blog avec module de recherche d'article." },
+        { img: 'miro/miro_3.jpg', title: 'Representation graphique de statistiques avec les éléments nécessaires.' },
+        { img: 'miro/miro_4.jpg', title: 'Classement des meilleurs ostéopathes par ville en fonction de leurs notations.' }
       ]
     }
   ]
