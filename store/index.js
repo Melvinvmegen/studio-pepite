beb59cea63e0fa9c5e5ec7dd84bf07b29c3fcafe
Melvin van Megen <melvin.vmegen@gmail.com>
2021-02-21 16:24:00 +0100

    UP (projects_filled) project pages improved

--- store/index.js
+++ store/index.js
@@ -4,8 +4,10 @@ export const state = () => ({
       title: 'le bioclimatique en co-living',
       link: 'le-bioclimatique-en-co-living',
       src: 'project_1.jpg',
-      desc: 'projet mémoire de fin d’étude en cours de construction … bientôt en ligne !',
+      description: 'projet mémoire de fin d’étude en cours de construction … bientôt en ligne !',
       projectImages: [
+        { img: 'project_1.jpg', title: 'Création de temoignage pour les utilisateurs identifié ou non.' },
+        { img: 'project_1.jpg', title: 'Création de temoignage pour les utilisateurs identifié ou non.' },
         { img: 'project_1.jpg', title: 'Création de temoignage pour les utilisateurs identifié ou non.' }
       ]
     },
@@ -13,7 +15,7 @@ export const state = () => ({
       title: 'l’antre de l’artiste',
       link: 'antre-de-l’artiste',
       src: 'project_1.jpg',
-      desc: `projet en freelance de transformation d’un viel appartement en bureau / atelier. désir de créer un lieu à l’identité 
+      description: `projet en freelance de transformation d’un vielle appartement en bureau / atelier. désir de créer un lieu à l’identité 
             industrielle et actuel en gardant une âme rétro.`,
       projectImages: [
         { img: 'antre_de_lartiste/ART_P1.jpg', title: 'Création de temoignage pour les utilisateurs identifié ou non.' },
@@ -25,19 +27,15 @@ export const state = () => ({
       title: 'la théorie du jeu',
       link: 'la-théorie-du-jeu',
       src: 'project_2.jpg',
-      desc: `la théorie des jeux est un projet de 4ème année d’école (réalisé en groupe de 4) de rénovation d’un bâtiment dont l’usage n’est plus 
+      description: `la théorie des jeux est un projet de 4ème année d’école (réalisé en groupe de 4) de rénovation d’un bâtiment dont l’usage n’est plus 
             défini en espace socio-culturel, actuel et accessible pour tous. Un centre social et culturel est un lieu de proximité à vocation globale, 
             familiale et intergénérationnelle, qui accueille toute la population en veillant à la mixité sociale. C’est un lieu d’animation de la vie sociale permettant 
             aux habitants d’exprimer, de concevoir et de réaliser leurs projets : il prend en compte l’expression des demandes et des initiatives des usagers et des habitants, 
-            et favorise la vie sociale et la vie associative. Il propose des activités ou des services à finalité sociale, éducative, culturelle ou de loisirs, 
-            ainsi que des actions spécifiques pour répondre aux problématiques sociales du territoire. 
-            Il favorise le développement des initiatives des usagers en mettant des moyens humains et logistiques à leur disposition. 
-            Pour le projet, nous devons rénover un bâtiment d’environ 3000m2 sur 2 étages. Il a fallu créer divers types d’espaces tels que : 
+            et favorise la vie sociale et la vie associative. <br><br>
+            Pour le projet, nous devions rénover un bâtiment d’environ 3000m2 sur 2 étages. Il a fallu créer divers types d’espaces tels que : 
             une galerie d’exposition, un café/brasserie, une bibliothèque, des lieux intimistes de travail, de réflexion & de recherche, 
             un atelier, un fab lab, une ludothèque, des salles de musiques & de vision, des espaces techniques… Nous avons axé notre concept 
-            sur l’intergénérationnalité et la favorisation des échanges : nous avons puisé notre inspiration dans les jeux de société (notemment le monopoly), 
-            le livre donnant-donnant de robert axelrod, une vie d’écart sur canal + et enfin le jeu de la dame sur netflix. nous avons 
-            utilisé les 4 couleurs associées aux jeux : le bleu, le rouge, le jaune et le vert ainsi que le damier.`,
+            sur l’intergénérationnalité et la favorisation des échanges.`,
       projectImages: [
         { img: 'theorie_du_jeu/GABARIT1.jpg', title: 'Création de temoignage pour les utilisateurs identifié ou non.' },
         { img: 'theorie_du_jeu/GABARIT2.jpg', title: "Mise en place d'un hub de blog avec module de recherche d'article." },
@@ -51,8 +49,8 @@ export const state = () => ({
       title: 'retrorama',
       link: 'retrorama',
       src: 'project_3.jpg',
-      desc: `rétrorama c’est le projet de rénovation d’un ancien batiment en hostel pour la chaine ho36. c’est un projet réalisé seule, 
-            en troisième année d’école. le principe du concept est de créer un lieu au style purement rétro-futuriste, 
+      description: `rétrorama c’est le projet de rénovation d’un ancien batiment en hostel pour la chaine ho36. c’est un projet réalisé seule, 
+            en troisième année d’école. <br>le principe du concept est de créer un lieu au style purement rétro-futuriste, 
             une invitation dans une autre dimension. la conception du lieu a été issue d’une réfléxion sur le temps et plus 
             particulièrement la relation entre le passé et le futur. un espace entre réalité et fiction dans lequel cohabitent 
             la fantaisie du passé et la magie du futur.`,
@@ -72,9 +70,9 @@ export const state = () => ({
       title: 'jacquemus',
       link: 'jacquemus',
       src: 'project_4.jpg',
-      desc: `la cagole, c’est le projet de création de la première boutique physique de la marque française de prêt-à-porter jacquemus. 
+      description: `la cagole, c’est le projet de création de la première boutique physique de la marque française de prêt-à-porter jacquemus. 
             le projet était orienté autour des valeurs de la marque (la provence, le sud, le soleil...). projet de troisième année d’école réalisé 
-            en groupe de 3. nous avons défini notre projet comme «simplicité sophistiquée». c’est la création d’un lieu atypique 
+            en groupe de 3. nous avons défini notre projet comme «simplicité sophistiquée». <br>c’est la création d’un lieu atypique 
             inspiré de la cagole marseillaise : éxagération, le trop, les couleurs, la sophistication, les courbes, les reflets... 
             un maître mot : l’extravagance. nos inspirations pour ce projet ont été : le film le mépris de jean-luc godard, 
             le documentaire cagole forever de sebastien haddouk,la bière la cagole d’yves darnaud. nous avons choisi d’utiliser 
@@ -91,9 +89,9 @@ export const state = () => ({
       title: 'dumbo stairs',
       link: 'dumbo-stairs',
       src: 'project_8.jpg',
-      desc: `participation (en équipe de 2, avec justin, ingénieur) à un concours de création d’un escalier en béton, 
+      description: `participation (en équipe de 2, avec justin, ingénieur) à un concours de création d’un escalier en béton, 
             organisé par l’entreprise bpm groupe. le produit présenté est un assemblage par câbles acier de blocs de béton 
-            bfup creusés afin de créer deux marches. un bloc de 2 marches en béton d’un volume de 0,0449m3 est composé de 
+            bfup creusés afin de créer deux marches. <br><br>un bloc de 2 marches en béton d’un volume de 0,0449m3 est composé de 
             béton fibré ayant une masse volumique de 2600 kg/m3. nous estimons donc la masse d’un module de béton à 116kg, 
             ce qui paraît correct du point de vue de réalisation et ancrage des cables.`,
       projectImages: [
@@ -103,11 +101,11 @@ export const state = () => ({
       ]
     },
     {
-      title: 'Jimi Hendrix',
-      link: 'Jimi Hendrix',
+      title: 'jimi Hendrix',
+      link: 'jimi Hendrix',
       src: 'project_5.jpg',
-      desc: `conception d’un lieu chaleureux dans lequel les clients peuvent se projeter. utilisation d’une oeuvre d’art représentant jimi 
-            hendrix par thomas krief. projet (réalisé en freelance) d’aménagement d’un appartement acquis en vefa. l’appartement se situe à lyon, 
+      description: `conception d’un lieu chaleureux dans lequel les clients peuvent se projeter. utilisation d’une oeuvre d’art représentant jimi 
+            hendrix par thomas krief. projet (réalisé en freelance) d’aménagement d’un appartement acquis en vefa. <br><br>l’appartement se situe à lyon, 
             dans le troisième arrondissement. l’appartement est déstiné à la location pour une petite famille. aménagement d’espaces verts 
             dans le salon et sur la terrasse. technique de définition des espaces par la couleurs.`,
       projectImages: [
@@ -122,8 +120,8 @@ export const state = () => ({
       title: 'revival',
       link: 'revival',
       src: 'project_6.jpg',
-      desc: `projet de rénovation d’une villa d’environ 200m2 à Marcy (69) pour une famille de 4 personnes. projet réalisé 
-            seule en deuxième année d’école .la tendance mid century etait fortement souhaitée notemment par l’utilisation 
+      description: `projet de rénovation d’une villa d’environ 200m2 à Marcy (69) pour une famille de 4 personnes. projet réalisé 
+            seule en deuxième année d’école. <br>la tendance mid century etait fortement souhaitée notemment par l’utilisation 
             de matériaux nobles (cuivre, pierre, bois, pièces d’art...). logiciels utilisés pour la réalisation de ce projet : 
             autocad, sketchup, excel.`,
       projectImages: [
@@ -142,8 +140,9 @@ export const state = () => ({
       title: 'miro x mondrian',
       link: 'miro-x-mondrian',
       src: 'project_7.jpg',
-      desc: `le projet ar(t)chitecture consistait en la rénovation d’un appartement déstiné à une location de courte durée. 
-             le maître d’ouvrage souhaitait un aménagement des plus atypiques.projet réalisé seule, en deuxième année d’école. 
+      description: `le projet ar(t)chitecture consistait en la rénovation d’un appartement déstiné à une location de courte durée. 
+             le maître d’ouvrage souhaitait un aménagement des plus atypiques. projet réalisé seule, en deuxième année d’école.
+             <br> 
              j’ai pris la décision de trouver un fil conducteur à mon projet et d’en faire un lieu avant-gardiste, unique. 
              mon inspiration proveneait des galeries d’art et du peintre joan miro. `,
       projectImages: [
